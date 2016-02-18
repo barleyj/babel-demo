dc8b9d945a269305d6c32e92cea2c7543f0985f1
Jayson Barley <jayson.barley@jivesoftware.com>
2016-02-17 20:45:05 -0800

    Adding some text to show that it works

--- docs/demo.py
+++ docs/demo.py
@@ -2,7 +2,7 @@ import urllib2
 import json
 
 def get_repos_for(user):
-    results = urllib2.urlopen('https://api.github.com/users/barleyj/repos')
+    results = urllib2.urlopen('https://api.github.com/users/{}/repos'.format(user))
     content = results.read()
     repos = json.loads(content)
     return [repo['name'] for repo in repos]
